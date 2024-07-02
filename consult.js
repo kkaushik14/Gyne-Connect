e14d641340eb9dc86881162c374840af3444eb99
kkaushik-24 <1ms22ee024@msrit.edu>
2024-07-03 03:08:20 +0530

    added form for booking

--- consult.js
+++ consult.js
@@ -1,9 +1,27 @@
-// document.querySelectorAll('.nav a').forEach(anchor => {
-//     anchor.addEventListener('click', function (e) {
-//         e.preventDefault();
+// form
+function genForm() {
+    var formPrompt = document.getElementById('appointmentForm');
+    formPrompt.classList.remove('hidden');
 
-//         document.querySelector(this.getAttribute('href')).scrollIntoView({
-//             behavior: 'smooth'
-//         });
-//     });
-// });
+    var formElement = document.getElementById('appointmentFormElement');
+    formElement.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        var name = document.getElementById('name_input').value;
+        var email = document.getElementById('email_input').value;
+        var age = document.getElementById('age_input').value;
+        var contact = document.getElementById('contact_input').value;
+        var scheduledDate = document.getElementById('scheduled_date_input').value;
+        var scheduledTime = document.getElementById('scheduled_time_input').value;
+
+        if (!name || !email || !age || !contact || !scheduledDate || !scheduledTime) {
+            alert('Please fill out all required fields.');
+            return;
+        }
+
+        formPrompt.classList.add('hidden');
+        alert('Your appointment is scheduled!');
+
+        formElement.reset();
+    });
+}
